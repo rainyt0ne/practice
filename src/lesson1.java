3b94cc71f9f5d598ac23de7dbd25de75ec60c040
rainyt0ne <rainyt0ne841@gmail.com>
2023-11-14 23:31:46 +0900

    commit 1

--- src/lesson1.java
+++ src/lesson1.java
@@ -1,5 +1,23 @@
-class lesson1 {
+class lesson1_1 {
     public static void main(String[] args) {
       System.out.println("Hello Java");
     }
+}
+
+class lesson1_2 {
+  public static void main(String[] args) {
+    int x;
+    x = 10;
+    System.out.println("x=" + x);
+  }
+}
+
+class lesson1_3 {
+  public static void main(String[] args) {
+    int x;
+    int y;
+    x = 10;
+    y = 15;
+    System.out.println("x="+x+",y="+y);
+  }
 }
