cdf94839a86ed6a992214288ef26e1ab8524bfce
rainyt0ne <rainyt0ne841@gmail.com>
2023-11-16 23:53:50 +0900

    commit 6

--- src/lesson1-2.java
+++ src/lesson1-2.java
@@ -41,3 +41,20 @@ class lesson1_5 {
   }
 }
 
+/*次のプログラムを作成しなさい。
+
+int 型の変数 x に数値 7 を代入する。
+変数 x の値を 3 倍にする。
+変数 x の値を表示する。
+変数 x の値を半分（1/2）にする。
+変数 x の値を表示する。 */
+class lesson1_6 {
+  public static void main(String[] args) {
+    int x;
+    x = 7;
+    x *= 3;
+    System.out.println("x="+x);
+    x /= 2;
+    System.out.println("x="+x);
+  }
+}
