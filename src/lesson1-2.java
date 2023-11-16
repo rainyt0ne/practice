de653d37275ce217ea42bf29da3386559db00b97
rainyt0ne <rainyt0ne841@gmail.com>
2023-11-16 23:55:59 +0900

    commit 12

--- src/lesson1-2.java
+++ src/lesson1-2.java
@@ -115,3 +115,16 @@ class lesson1_11 {
   }
 }
 
+/*int 型の変数 x に任意の数値を代入し、インクリメント演算子、デクリメント演算子を適用して結果を表示することにより演算子の効果を確認するプログラムを作成しなさい。 */
+class lesson1_12 {
+  public static void main(String[] args) {
+    int x;
+    x = 10;
+    x++;
+    System.out.println(x);
+    
+    x--;
+    x--;
+    System.out.println(x);
+  }
+}
