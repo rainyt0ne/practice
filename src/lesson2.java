4ea1d928d687b55bc78b45bc07801a2f100d292f
rainyt0ne <rainyt0ne841@gmail.com>
2023-11-16 23:49:22 +0900

    Initial commit

---
+++ src/lesson2.java
@@ -0,0 +1,10 @@
+public class Lesson2-1 {
+    public static void main(String[] args) {
+        int dice = (int)(Math.random()*7)+1;
+        System.out.println("さいころの目："+dice);
+
+        if (1 <= dice && dice <= 6) {
+            
+            
+        }
+}
