40091b8ffdcd379255493dabec215ac0fa25ff1c
rainyt0ne <rainyt0ne841@gmail.com>
2023-11-16 23:52:44 +0900

    commit 4

--- src/lesson1-2.java
+++ src/lesson1-2.java
@@ -25,3 +25,12 @@ class lesson1_3 {
   }
 }
 
+/*int 型の変数 x に数値 13 と 17 の和を代入し、x の値を表示するプログラムを作成しなさい。 */
+class lesson1_4 {
+  public static void main(String[] args) {
+    int x;
+    x = 13 + 17;
+    System.out.println(x);
+  }
+}
+
