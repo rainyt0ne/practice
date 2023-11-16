8fdf48de2a202e06a128837b9408dc5733191fba
rainyt0ne <rainyt0ne841@gmail.com>
2023-11-16 23:51:07 +0900

    commit 2

--- src/lesson1-2.java
+++ src/lesson1-2.java
@@ -4,3 +4,13 @@ class lesson1_1 {
       System.out.println("Hello Java");
     }
 }
+
+/*int 型の変数 x に数値 11 を代入し、x の値を“x=11”のように表示するプログラムを作成しなさい。 */
+class lesson1_2 {
+  public static void main(String[] args) {
+    int x;
+    x = 11;
+    System.out.println("x=" + x);
+  }
+}
+
