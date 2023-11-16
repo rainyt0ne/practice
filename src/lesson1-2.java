0f9c9ec285f901cea0fd70951d3828f16c0448c2
rainyt0ne <rainyt0ne841@gmail.com>
2023-11-16 23:54:31 +0900

    commit 8

--- src/lesson1-2.java
+++ src/lesson1-2.java
@@ -75,3 +75,15 @@ class lesson1_7 {
   }
 }
 
+/*int 型の変数 x、y に数値 19、23 を代入し、その積を変数 z に代入して z の値を表示するプログラムを作成しなさい。 */
+class lesson1_8 {
+  public static void main(String[] args) {
+    int x, y, z;
+    x = 19;
+    y = 23;
+    z = x * y;
+
+    System.out.println("z="+z);
+  }
+}
+
