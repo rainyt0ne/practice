61496492a8b830a5f0c017625d0cb385c2dd2fb6
rainyt0ne <rainyt0ne841@gmail.com>
2023-11-16 23:52:15 +0900

    commit 3

--- src/lesson1-2.java
+++ src/lesson1-2.java
@@ -14,3 +14,14 @@ class lesson1_2 {
   }
 }
 
+/*int 型の変数 x、y に数値 13、17 を代入し、x、y の値を“x=13,y=17”のように表示するプログラムを作成しなさい。 */
+class lesson1_3 {
+  public static void main(String[] args) {
+    int x;
+    int y;
+    x = 13;
+    y = 17;
+    System.out.println("x="+x+",y="+y);
+  }
+}
+
