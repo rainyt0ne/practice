94205cbe0b2e0f6925d2479d8fd7f127549f8e33
rainyt0ne <rainyt0ne841@gmail.com>
2023-11-16 23:55:19 +0900

    commit 10

--- src/lesson1-2.java
+++ src/lesson1-2.java
@@ -96,3 +96,12 @@ class lesson1_9 {
   }
 }
 
+/*int 型の変数 x に任意の数値を代入し、x を 1 乗、2 乗、3 乗した結果を表示するプログラムを作成しなさい。 */
+class lesson1_10 {
+  public static void main(String[] args) {
+    int x;
+    x = 10;
+    System.out.println(x+","+x*x+","+x*x*x);
+  }
+}
+
