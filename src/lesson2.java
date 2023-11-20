d5991c1685d37a12c035038fcb771ea8cfdadceb
rainyt0ne <rainyt0ne841@gmail.com>
2023-11-20 22:35:07 +0900

    commit 2-1

--- src/lesson2.java
+++ src/lesson2.java
@@ -20,17 +20,20 @@ class lesson2_1 {
 class lesson2_2 {
     public static void main(String[] args) {
         Random rnd = new Random();
-        int num = rnd.nextInt(4)+1;
+        int num = rnd.nextInt(4);
         switch (num) {
             case 1:
-            System.out.println("1です");
-            break;
+                System.out.println("1です");
+                break;
             case 2:
-            System.out.println("2です");
-            break;
+                System.out.println("2です");
+                break;
             case 3:
-            System.out.println("3です");
-            break;
+                System.out.println("3です");
+                break;
+            default:
+                System.out.println(num+"です");
+                break;
         }
     }
 }
