9e296197244e22a59a645828f4ccb0b96cda71ab
rainyt0ne <rainyt0ne841@gmail.com>
2023-11-22 23:57:05 +0900

    commit 6

--- src/lesson2.java
+++ src/lesson2.java
@@ -68,3 +68,14 @@ class lesson2_5 {
         }
     }
 }
+
+class lesson2_6 {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i--) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
