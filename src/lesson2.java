187032a5934242971fcf0bfbafa1a6c607263d10
rainyt0ne <rainyt0ne841@gmail.com>
2023-11-23 21:26:02 +0900

    commit 11

--- src/lesson2.java
+++ src/lesson2.java
@@ -115,3 +115,22 @@ class lesson2_10 {
         }
     }
 }
+
+class lesson2_11 {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5,6,7,8,9};
+
+        int oddSum = 0;
+        int evenSum = 0;
+
+        for (int number: numbers) {
+            if (number % 2 == 0) {
+                evenSum += number;
+                System.out.println("偶数の和は"+evenSum+"です");
+                continue;
+            }
+            oddSum += number;
+            System.out.println("奇数の和は"+oddSum+"です");
+        }
+    }
+}
