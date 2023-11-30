8be62e048280f2c597a109455493a5a0169d9680
rainyt0ne <rainyt0ne841@gmail.com>
2023-11-30 22:54:26 +0900

    commit 2

--- src/lesson2-2.java
+++ src/lesson2-2.java
@@ -15,3 +15,24 @@ class lesson2_2 {
         }
     }
 }
+
+class lesson2_2 {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int num = rnd.nextInt(4);
+        switch (num) {
+            case 1:
+                System.out.println("1です");
+                break;
+            case 2:
+                System.out.println("2です");
+                break;
+            case 3:
+                System.out.println("3です");
+                break;
+            default:
+                System.out.println(num+"です");
+                break;
+        }
+    }
+}
