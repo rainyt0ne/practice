3031b5b77469529ef0b5ac5c53ebeedc3ae3fc50
rainyt0ne <rainyt0ne841@gmail.com>
2023-11-30 22:53:56 +0900

    commit 1

---
+++ src/lesson2-2.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+class lesson2_2 {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int x, num;
+        x = rnd.nextInt(10);
+        num = 5;
+        if (num > x) {
+            System.out.println("numの勝ち。"+"ちなみにxの数値は"+x+"でした！");
+        } else if (num == x) {
+            System.out.println("numと同じ。"+"ちなみにxの数値は"+x+"でした！");
+        } else {
+            System.out.println("numの負け。"+"ちなみにxの数値は"+x+"でした！");
+        }
+    }
+}
